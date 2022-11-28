90c37aa13e1ec99fd2c5fab4e7cbdf488475de71
CodeyBrodey <102107941+CodeyBrodey@users.noreply.github.com>
2022-11-28 13:59:26 -0300

    Challenge complete

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log('It works!');
+const numbers = document.querySelectorAll('.rating-numbers');
+const numbersArray = Array.from(numbers);
+
+const setToActive = (event) => {
+    const active = event.target;
+    const unactive = numbersArray.filter(number => {
+        return (number !== active)
+    })
+    unactive.forEach(unactiveEl => {
+        unactiveEl.classList.remove('selected-rating')
+        unactiveEl.classList.add('rating-numbers')
+    })
+    active.classList.add('selected-rating');
+    active.classList.remove('rating-numbers')
+}
+
+numbers.forEach(number => {
+    number.addEventListener('click', setToActive)
+})
+
+
+const submission = document.querySelector('.submit-button');
+
+const getSelectedRating = (event) => {
+    const cardSelection = document.querySelector('.card-selection')
+    const cardSubmitted = document.querySelector('.card-submitted')
+    const selectedNumber = document.querySelector('.selected-rating')
+    const feedbackVar = document.querySelector('.feedback-var')
+    cardSelection.classList.add('not-visible')
+    cardSubmitted.classList.remove('not-visible')
+    feedbackVar.textContent = selectedNumber.textContent;
+}
+
+submission.addEventListener('click', getSelectedRating);
